2408a8f5bba8495cce459f49454c99eb57b7e824
sosssoldi <angus_67@live.it>
2017-04-15 10:55:03 +0200

    Aggiornata descrizione test UserService

--- src/test/test_UsersService.js
+++ src/test/test_UsersService.js
@@ -10,33 +10,33 @@ describe('Back-end', function(done)
     {
       describe('addUser', function(done)
       {
-        it("Nel caso in cui si verifichi un errore, il campo statusCode della risposta deve essere impostato a 500");
-        it("Nel caso in cui non si verifichino errori, il campo statusCode della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere la lista dei canali Slack (utenti, canali pubblici e gruppi privati) in formato JSON");
-        it("Nel caso in cui sia passato un parametro non atteso, il campo statusCode della risposta deve essere impostato a 400");
+        it("Nel caso in cui si verifichi un errore, il campo \file{statusCode} della risposta deve essere impostato a 500");
+        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200");
+        it("Nel caso in cui sia passato un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400");
       });
       describe('deleteUser', function(done)
       {
-        it("Nel caso in cui si verifichi un errore, il campo statusCode della risposta deve essere impostato a 500");
-        it("Nel caso in cui non si verifichino errori, il campo statusCode della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere la lista dei canali Slack (utenti, canali pubblici e gruppi privati) in formato JSON");
-        it("Nel caso in cui sia passato un parametro non atteso, il campo statusCode della risposta deve essere impostato a 400");
+        it("Nel caso in cui si verifichi un errore, il campo \file{statusCode} della risposta deve essere impostato a 500");
+        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200");
+        it("Nel caso in cui sia passato un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400");
       });
       describe('getUser', function(done)
       {
-        it("Nel caso in cui si verifichi un errore, il campo statusCode della risposta deve essere impostato a 500");
-        it("Nel caso in cui non si verifichino errori, il campo statusCode della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere la lista dei canali Slack (utenti, canali pubblici e gruppi privati) in formato JSON");
-        it("Nel caso in cui sia passato un parametro non atteso, il campo statusCode della risposta deve essere impostato a 400");
+        it("Nel caso in cui si verifichi un errore, il campo \file{statusCode} della risposta deve essere impostato a 500");
+        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere l'utente richiesto");
+        it("Nel caso in cui sia passato un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400");
       });
       describe('getUserList', function(done)
       {
-        it("Nel caso in cui si verifichi un errore, il campo statusCode della risposta deve essere impostato a 500");
-        it("Nel caso in cui non si verifichino errori, il campo statusCode della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere la lista dei canali Slack (utenti, canali pubblici e gruppi privati) in formato JSON");
-        it("Nel caso in cui sia passato un parametro non atteso, il campo statusCode della risposta deve essere impostato a 400");
+        it("Nel caso in cui si verifichi un errore, il campo \file{statusCode} della risposta deve essere impostato a 500");
+        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere la lista degli utenti");
+        it("Nel caso in cui sia passato un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400");
       });
       describe('updateUser', function(done)
       {
-        it("Nel caso in cui si verifichi un errore, il campo statusCode della risposta deve essere impostato a 500");
-        it("Nel caso in cui non si verifichino errori, il campo statusCode della risposta deve essere impostato a 200 ed il corpo della risposta deve contenere la lista dei canali Slack (utenti, canali pubblici e gruppi privati) in formato JSON");
-        it("Nel caso in cui sia passato un parametro non atteso, il campo statusCode della risposta deve essere impostato a 400");
+        it("Nel caso in cui si verifichi un errore, il campo \file{statusCode} della risposta deve essere impostato a 500");
+        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200");
+        it("Nel caso in cui sia passato un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400");
       });
     });
   });
