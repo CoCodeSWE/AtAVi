9e58b0026182043e0db2f528a7ffca13d8e968d5
sime1 <sime.pizzi@gmail.com>
2017-04-16 23:28:00 +0200

    test userservice

--- src/test/test_UsersService.js
+++ src/test/test_UsersService.js
@@ -2,8 +2,11 @@ const UsersService = require('../Back-end/Users/UsersService');
 const chai = require('chai');
 const expect = chai.expect;
 
+const context = require('./stubs/LambdaContext');
+
 describe('Back-end', function(done)
 {
+  service = ... //dependency injection stub
   describe('Users', function(done)
   {
     describe('UsersService', function(done)
@@ -11,7 +14,21 @@ describe('Back-end', function(done)
       describe('addUser', function(done)
       {
         it("Nel caso in cui si verifichi un errore, il campo \file{statusCode} della risposta deve essere impostato a 500");
-        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200");
+        it("Nel caso in cui non si verifichino errori, il campo \file{statusCode} della risposta deve essere impostato a 200", function()
+        {
+          let user=
+          {
+            name: "Mauro",
+            username: "mou"
+          };
+          let ev = { body: JSON.stringfy(user); }
+          service.addUser(ev, context);
+          let call = context.succeed.getCall(0);
+
+          expect(context.succeed.calledOnce).to.be.true;
+          expect(call.args[0]).not.to.be.null;
+          expect(call.args[0]).to.be.deep.equal({body:{}, statusCode: 200});
+        });
         it("Nel caso in cui sia passato un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400");
       });
       describe('deleteUser', function(done)
