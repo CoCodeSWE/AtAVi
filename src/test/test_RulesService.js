6fd24504091084b93e64d6386621f059a4bdf1e5
Scramasax <scramasax443@gmail.com>
2017-04-17 19:49:44 +0200

    addUser

--- src/test/test_RulesService.js
+++ src/test/test_RulesService.js
@@ -1,8 +1,9 @@
 const RulesService = require('../Back-end/Rules/RulesService');
 const chai = require('chai');
 const expect = chai.expect;
-const task = require('./stubs/TasksDAO');
-const rules = require('./stubs/RulesDAO');
+const taskDAO = require('./stubs/TasksDAO');
+const rulesDAO = require('./stubs/RulesDAO');
+const context = require('./stubs/LambdaContext');
 
 describe('Back-end', function(done)
 {
@@ -10,13 +11,88 @@ describe('Back-end', function(done)
   {
     describe('RulesService', function(done)
     {
-      let rule = new RulesService(task,rules);
+      beforeEach(function()
+      {
+      let rules = new RulesService(taskDAO,rulesDAO);
+    });
       describe('addRule', function(done)
       {
-        it("Nel caso in cui la chiamata al metodo venga fatta con un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400.");
-        it("Nel caso in cui la chiamata al metodo generi un errore del microservizio, il campo \file{statusCode} della risposta deve essere impostato a 500.");
-        it("Nel caso in cui la chiamata al metodo vada a buon fine, il campo \file{statusCode} della risposta deve essere impostato a 200.");
+        it("Nel caso in cui la chiamata al metodo venga fatta con un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400.",function(done)
+        {
+          let ev =
+          {
+            body:''
+          };
+
+          rules.addUser(ev,context);
+          let call = context.succeed.getCall(0);
+          expect(context.succeed.calledOnce).to.be.true;
+          expect(call.args[0]).not.to.be.null;
+          expect(call.args[0]).to.be.deep.equal({body:{}, statusCode: 400; done();});
+
+        });
+        it("Nel caso in cui la chiamata al metodo generi un errore del microservizio, il campo \file{statusCode} della risposta deve essere impostato a 500.",function(done)
+        {
+          let rule =
+          {
+            "enabled": false,
+            "id": 1,
+            "name" : "testRule",
+            "targets":
+              [
+                {
+                  "company": "testCompany",
+                  "member": "testMember",
+                  "name": "testName"
+                }
+              ],
+            "task":
+              {
+                "params":
+                  {
+                    "param":"testParam"
+                  },
+                "task": "testTask"
+            }
+          };
+          let ev =  { body: JSON.stringfy(rule); }
+          rules.addUser(ev,context);
+          let call = context.succeed.getCall(0);
+          expect(context.succeed.calledOnce).to.be.true;
+          expect(call.args[0]).not.to.be.null;
+          expect(call.args[0]).to.be.deep.equal({body:{}, statusCode: 500; done();});
       });
+        it("Nel caso in cui la chiamata al metodo vada a buon fine, il campo \file{statusCode} della risposta deve essere impostato a 200.",function(done)
+        {
+          let rule =
+          {
+            "enabled": false,
+            "id": 1,
+            "name" : "testRule",
+            "targets":
+              [
+                {
+                  "company": "testCompany",
+                  "member": "testMember",
+                  "name": "testName"
+                }
+              ],
+            "task":
+              {
+                "params":
+                  {
+                    "param":"testParam"
+                  },
+                "task": "testTask"
+            }
+          };
+          let ev =  { body: JSON.stringfy(rule); }
+          rules.addUser(ev,context);
+          let call = context.succeed.getCall(0);
+          expect(context.succeed.calledOnce).to.be.true;
+          expect(call.args[0]).not.to.be.null;
+          expect(call.args[0]).to.be.deep.equal({body:{}, statusCode: 200; done();});
+        });
       describe('deleteRule', function(done)
       {
         it("Nel caso in cui la chiamata al metodo venga fatta con un parametro non atteso, il campo \file{statusCode} della risposta deve essere impostato a 400.");
