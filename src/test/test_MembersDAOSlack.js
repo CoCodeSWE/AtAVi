db5177b1a7b4dcb6eae6bf4cf4dc393d37f9d7c7
ntintorri <tintorri.nicola@gmail.com>
2017-04-18 01:28:15 +0200

    Aggiunto test getMember

--- src/test/test_MembersDAOSlack.js
+++ src/test/test_MembersDAOSlack.js
@@ -38,14 +38,90 @@ describe('Back-end', function()
 
 			describe('getMember', function()
 			{
-				it('Nel caso in cui il metodo ottenga il membro dell\'azienda allora l\'Observable invia tale Member all\'Observer iscritto tramite il metodo next e lo notifica richiamando una sola volta il metodo complete.');
-				it('Se si verifica un errore nell\'ottenere il membro dell\'azienda, l\'Observable deve notificare l\'Observer iscritto richiamando il metodo error.');
+				it('Nel caso in cui il metodo ottenga il membro dell\'azienda allora l\'Observable invia tale Member all\'Observer iscritto tramite il metodo next e lo notifica richiamando una sola volta il metodo complete.', function()
+				{
+					members.getMember('mou').subscribe(
+					{
+						next: next,
+						error: error,
+						complete: complete
+					});
+					
+					//Docs: https://api.slack.com/methods/users.info
+					let res =
+					{
+						'ok': true,
+						'user': 
+						{
+							'id': 'U023BECGF',
+							'name': 'bobby',
+							'deleted': false,
+							'color': '9f69e7',
+							'profile':
+							{
+								'avatar_hash': 'ge3b51ca72de',
+								'current_status': ':mountain_railway: riding a train',
+								'first_name': 'Bobby',
+								'last_name': 'Tables',
+								'real_name': 'Bobby Tables',
+								'email': 'bobby@slack.com',
+								'skype': 'my-skype-name',
+								'phone': '+1 (123) 456 7890'
+							},
+							'is_admin': true,
+							'is_owner': true,
+							'updated': 1490054400,
+							'has_2fa': true
+						}
+					};
+					
+					web_client.info.yield(res);
+					
+					expect(error.callCount).to.equal(0);
+					
+					expect(next.callCount).to.equal(1);
+					let callNext = next.getCall(0);
+					expect(callNext.args[0].id).to.equal(id);
+					expect(callNext.args[1].id).to.equal(name);
+					
+					expect(complete.callCount).to.equal(1);
+				});
+				
+				it('Se si verifica un errore nell\'ottenere il membro dell\'azienda, l\'Observable deve notificare l\'Observer iscritto richiamando il metodo error.', function()
+				{
+					members.getMember('mou').subscribe(
+					{
+						next: next,
+						error: error,
+						complete: complete
+					});
+					
+					//Esempio di errore (https://api.slack.com/methods/users.info/test)
+					let res = 
+					{
+						'ok': false,
+						'error': 'not_authed'
+					}
+					
+					web_client.info.yield(res);
+					
+					expect(error.callCount).to.equal(1);
+					let callError = error.getCall(0);
+					expect(callError.args[0].ok).to.equal(false);
+					expect(callError.args[0].error).to.equal('not_authed');
+					
+					expect(next.callCount).to.equal(0);
+					expect(complete.callCount).to.equal(0);	
+				});
 			});
 
 			describe('getMemberList', function()
 			{
 				it('L\'Observable deve notificare l\'Observer con il metodo complete solo dopo aver inviato tutti i blocchi di Member tramite il metodo next.');
+				//res.ok = true;
+				
 				it('Se si verifica un errore nell\'ottenere la lista dei membri dell\'azienda, l\'Observable deve notificare l\'Observer iscritto richiamando il metodo error.');
+				//res.ok = false;
 			});
 
 			describe('removeMember', function()
