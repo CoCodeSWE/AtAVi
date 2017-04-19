ffc64e7b89f94ffc21f66fa473c94fac9900487d
BottCode <mattiabottaro9@gmail.com>
2017-04-19 18:30:51 +0200

    appreso e scritto codice descrittivo dell'interazione con STT di IBM e api.ai

--- src/Back-end/APIGateway/VocalAPI.js
+++ src/Back-end/APIGateway/VocalAPI.js
@@ -1,3 +1,11 @@
+const http = require("http");
+const rp = require('request-promise');
+//var sys = require('sys');
+//var base64_decode = require('base64').decode;
+var fs = require('fs');
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+
+
 class VocalAPI
 {
   constructor(vocal, jwt, rp, stt, sns)
@@ -9,35 +17,189 @@ class VocalAPI
     this.jwt = jwt;
   }
 
-  query(event, context)
-  {
-    let body = JSON.parse(event.body);
-    let action = Math.floor(Math.random() * 2);
-    let result;
-    switch(action)
-    {
-      case 0:
-        result = this.method0();
-        break;
-      case 1:
-        result = this.method1();
-        break;
-      default:
-        result = 'default';
+  static queryLambda(event, context){
+    //let body=JSON.parse(event.body);
+
+    // CONTATTO IBM WATSON
+
+/* audio -> Base64 è fatto nel client
+    var buf = new Buffer('./hello4.wav');
+
+    sys.print(base64_encode(buf));
+
+    sys.print(base64_decode('hello_audio'));
+*/
+/*
+    ########## QUESTE COSE DEVONO ESSERE FATTE DA STTMODULE E VIRTUALASSISTANT #############
+
+    let action, text_request, text_response;
+    let options_api_ai = {
+      method: 'POST',
+      uri: 'https://api.api.ai/v1/query',
+      headers: {
+        'Content-Type':'application/json; charset=utf-8',
+        'Host':'api.api.ai',
+        'X-Amz-Date':'20170419T123358Z',
+        'Authorization':'Bearer 8e48b54bd55d4d68a68371a870558470'
+      },
+      body: {
+        query: [
+            '' // verrà stabilità da STT IBM
+        ],
+        timezone: 'Europe/Rome',
+        lang: 'en',
+        sessionId: '1234567890'
+      },
+      json: true
     }
-    console.log(result, action);
-    context.succeed({statusCode: 200, body: result});
+
+    let speech_to_text = new SpeechToTextV1({
+      username: '637a5774-9755-4b80-b134-af716927bc9d',
+      password: 'QfOX3P4ZlndK'
+    });
+
+    let params = {
+      audio: fs.createReadStream('./hello.wav'),
+      content_type: 'audio/wav'
+    };
+
+
+    speech_to_text.recognize(params, function(err, res) {
+      if (err)
+        console.log(err);
+      else
+      {
+        options_api_ai.body.query=res.results[0].alternatives[0].transcript; // query api.ai = testo ritornato da STT
+        // CONTATTO API.AI
+        rp(options_api_ai).then(function(parsedBody){
+          //console.log("Q: "+parsedBody.result.resolvedQuery+'\nA: '+parsedBody.result.speech);
+          action=parsedBody.result.action;
+          text_request=parsedBody.result.resolvedQuery;
+          text_response=parsedBody.result.speech;
+        }).catch(function(err){
+          console.log(err);
+        });
+      }
+    });
+    #########################################
+*/
+
+    switch(action){
+      case 'rule.add':
+        this._addRule();
+        break;
+      case 'rule.get':
+        this._getRule();
+        break;
+      case 'rule.getList':
+        this._getRuleList();
+        break;
+      case 'rule.remove':
+        this._removeRule();
+        break;
+      case 'rule.update':
+        this._updateRule();
+        break;
+      case 'user.add':
+        this._addUser();
+        break;
+      case 'user.addEnrollment':
+        this._addUserEnrollment();
+        break;
+      case 'user.get':
+        this._getUser();
+        break;
+      case 'user.getList':
+        this._getUserList();
+        break;
+      case 'user.login':
+        this._loginUser();
+        break;
+      case 'user.remove':
+        this._removeUser();
+        break;
+      case 'user.resetEnrollment':
+        this._resetUserEnrollment();
+        break;
+      case 'user.update':
+        this._updateUser();
+        break;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+          "action":action,
+          "res":{
+              "contexts":['nisba',"two"],
+              "data":{
+                  "token":"TOKEN"
+              },
+              "text_request": text_request,
+              "text_response":text_response
+          },
+          "session_id":"123"
+      }),
+    };
   }
 
-  method0()
-  {
-    return 'method 0';
+  context.succeed(response);
+
+  /******* RULE *********/
+
+  _addRule(rule){
   }
 
-  method1()
-  {
-    return 'method1';
+  _getRule(id){ // id è una stringa
+
   }
+  _getRuleList(){
+
+  }
+
+  _removeRule(id){
+
+  }
+
+  _updateRule(rule){
+
+  }
+
+  /******* USER *********/
+
+  _addUser(user){
+  }
+
+  _addUserEnrollment(enr){
+
+  }
+
+  _getUser(username){
+
+  }
+
+  _getUserList(){
+
+  }
+
+  _loginUser(enr){
+
+  }
+
+  _removeUser(username){
+
+  }
+
+  _resetUserEnrollment(username){
+
+  }
+
+  _updateUser(user){
+
+  }
+
 }
+
+VocalAPI.queryLambda();
 
 module.exports = VocalAPI;
--- src/Back-end/APIGateway/handler.js
+++ src/Back-end/APIGateway/handler.js
@@ -1,21 +1,80 @@
 //sarebbero tutti require, quelli che dobbiamo fare noi li lascio oggetti vuoti
+
+/*
+INPUT SCHEMA
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "VARequestAPIBody",
+  "type": "object",
+  "properties":{
+    "app":{"type":"string"},
+    "audio":{"type":"string"},
+    "data":{
+        "type":"array",
+        "items":{
+            "type":"string"
+        }
+    },
+    "session_id":{"type":"string"}
+  }
+}
+
+OUTPUT schema
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "VAResponse",
+  "type": "object",
+  "properties":{
+    "action":{"type":"string"},
+    "res":{
+        "type":"object",
+        "properties":{
+            "contexts":{"type":"array"},
+            "data":{"type":"object"},
+            "text_request":{"type":"string"},
+            "text_response":{"type":"string"}
+        }
+    },
+    "session_id":{"type":"string"}
+  }
+}
+*/
+
 'use strict';
 
-const sns = {}/*require('aws-sdk').SNS;*/
+//const sns = require('aws-sdk').SNS;
+const sns = {};
 const stt = {};
 const vocal = {};
-const rp = require('request-promise');
-const jwt = require('jsonwebtoken');
+//const rp = require('request-promise');
+const rp = {};
+const jwt = require('json-web-token');
 const VocalAPI = require('./VocalAPI');
 
 let gateway = new VocalAPI(vocal, jwt, rp, stt, sns);
 
-module.exports.query = gateway.query.bind(gateway);
+// module.exports.query = gateway.queryLambda.bind(gateway);
 
-/* ALTERNATIVA SENZA BIND
-module.exports.query = function(event, context)
+///* ALTERNATIVA SENZA BIND
+module.exports.queryLambda = function(event, context)
 {
-  gateway.query(event, context);
+  gateway.queryLambda(event, context);
+
+  /*const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+        "action":"ACT",
+        "res":{
+            "contexts":["one","two"],
+            "data":{
+                "token":"TOKEN"
+            },
+            "text_request":"question",
+            "text_response":"answer"
+        },
+        "session_id":"123"
+    }),
+  };*/
 }
 
-E' EQUIVALENTE*/
+// E' EQUIVALENTE*/
