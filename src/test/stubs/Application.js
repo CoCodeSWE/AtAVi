d83ed28b665498666a522ae45f27121b34ed9825
pier <tricomipierpaolo@gmail.com>
2017-04-19 23:29:13 +0200

    aggiunti test state e manager un po'

---
+++ src/test/stubs/Application.js
@@ -0,0 +1,6 @@
+const sinon = require('sinon');
+module.exports = {
+  getUI: sinon.stub(),
+  runCmd: sinon.stub()
+  
+}
--- src/test/test_Manager.js
+++ src/test/test_Manager.js
@@ -1,5 +1,8 @@
 const Manager = require('../Client/ApplicationManager/Manager');
 const chai = require('chai');
+const ApplicationRegistryClient = require ('./stubs/ApplicationRegistryClient')
+const HTMLElement = require(); //????????????????????????????
+const State = require ('./stubs/State');
 
 describe('Client', function()
 {
@@ -9,13 +12,25 @@ describe('Client', function()
     {
       describe('runApplication', function()
       {
-        it('Nel caso in cui l’applicazione è presente all\'interno di \file{State}, non viene interrogato il Client.');
-        it('Nel caso in cui l’applicazione non è presente all\'interno di \file{State}, viene interrogato il Client per ottenerla e la vecchia applicazione viene salvata nello \file{State}.');
+        it('Nel caso in cui l’applicazione sia presente all\'interno di \file{State}, non viene interrogato il Client.',function()
+        {
+          NON SO QUALE APPLICAZIONE RICERCARE D: prima inserisco e poi controllo
+
+        };
+
+        it('Nel caso in cui l’applicazione non sia presente all\'interno di \file{State}, viene interrogato il Client per ottenerla e la vecchia applicazione viene salvata nello \file{State}.');
       });
 
       describe('setFrame', function()
       {
-        it('Deve chiamare \file{appendChild} sul parametro passato al metodo per poter mostrare l’interfaccia utente.');
+        it('Deve chiamare \file{appendChild} sul parametro passato al metodo per poter mostrare l’interfaccia utente.', function()
+        {
+          manager = new Manager(ApplicationRegistryClient, 'element');
+          let call = manager.setFrame.getCall(0);
+          expect(manager.setFrame.calledOnce).to.be.true;
+          expect(call.args[0]).to.not.be.null;
+          expect(call.args[0].appendChild.calledOnce).to.be.true;
+        });
       });
     });
   });
