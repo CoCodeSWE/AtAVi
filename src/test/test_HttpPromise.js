d43504daeb623affa580706629173aaa433195e2
pier <tricomipierpaolo@gmail.com>
2017-04-20 21:53:17 +0200

    aggiunti test vari

--- src/test/test_HttpPromise.js
+++ src/test/test_HttpPromise.js
@@ -1,4 +1,4 @@
-const Manager = require('../Client/Logic/HttpPromise');
+const HttpPromise = require('../Client/Logic/HttpPromise');
 const chai = require('chai');
 
 describe('Client', function()
@@ -9,8 +9,23 @@ describe('Client', function()
     {
       describe('then', function()
       {
-        it('Se la richiesta va a buon fine, viene chiamata la funzione di callback \file{fulfill}.');
-        it('Se la richiesta fallisce, viene chiamata la funzione di callback \file{reject}.');
+        it('Se la richiesta va a buon fine, viene chiamata la funzione di callback \file{fulfill}.',function()
+        {
+          let httppromise = new HttpPromise('method','headers','[]',{});
+          let fulfill = function();
+          let reject = function();
+          let call = httppromise.then(fulfill, reject);
+          expect(call.args[0].calledOnce).to.be.true;
+        });
+
+        it('Se la richiesta fallisce, viene chiamata la funzione di callback \file{reject}.',function()
+        {
+          let httppromise = new HttpPromise('method','headers','[]',{});
+          let fulfill = function();
+          let reject = function();
+          let call = httppromise.then(fulfill, reject);
+          expect(call.args[1].calledOnce).to.be.true;
+        });
       });
     });
   });
--- src/test/test_Manager.js
+++ src/test/test_Manager.js
@@ -1,8 +1,9 @@
 const Manager = require('../Client/ApplicationManager/Manager');
 const chai = require('chai');
 const ApplicationRegistryClient = require ('./stubs/ApplicationRegistryClient')
-const HTMLElement = require(); //????????????????????????????
 const State = require ('./stubs/State');
+const Application = require('./stubs/Application');
+const HTMLElement = require('./stubs/HTMLElement');
 
 describe('Client', function()
 {
@@ -14,18 +15,30 @@ describe('Client', function()
       {
         it('Nel caso in cui l’applicazione sia presente all\'interno di \file{State}, non viene interrogato il Client.',function()
         {
-          NON SO QUALE APPLICAZIONE RICERCARE D: prima inserisco e poi controllo
-
+          //testo con applicazione che ho aggiunto io
+          state.addApp(Application, 'app');
+          let appltemp = state.getApp('app');
+          expect(Application).to.deep.equal(appltemp);
+          expect(registry_client.query.calledOnce).to.not.be.true;
         };
 
-        it('Nel caso in cui l’applicazione non sia presente all\'interno di \file{State}, viene interrogato il Client per ottenerla e la vecchia applicazione viene salvata nello \file{State}.');
+        it('Nel caso in cui l’applicazione non sia presente all\'interno di \file{State}, viene interrogato il Client per ottenerla e la vecchia applicazione viene salvata nello \file{State}.',function()
+        {
+          //testo con l'app nonEsistente che si suppone non esista
+          let appltemp = state.getApp('nonEsistente');
+          expect(appltemp).to.be.equal(null);
+          expect(state.addApp.calledOnce).to.be.true;
+          expect(registry_client.query.calledOnce).to.be.true;
+
+        });
+
       });
 
       describe('setFrame', function()
       {
         it('Deve chiamare \file{appendChild} sul parametro passato al metodo per poter mostrare l’interfaccia utente.', function()
         {
-          manager = new Manager(ApplicationRegistryClient, 'element');
+          manager = new Manager(ApplicationRegistryClient, HTMLElement);
           let call = manager.setFrame.getCall(0);
           expect(manager.setFrame.calledOnce).to.be.true;
           expect(call.args[0]).to.not.be.null;
