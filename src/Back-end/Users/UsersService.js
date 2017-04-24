f2bed2e452231a22ecdb05719356ad14bfe0ea79
ntintorri <tintorri.nicola@gmail.com>
2017-04-24 18:35:01 +0200

    Terminato UsersDAODynamoDB.js

--- src/Back-end/Users/UsersService.js
+++ src/Back-end/Users/UsersService.js
@@ -1,6 +1,43 @@
 class UsersService
 {
+	constructor(user)
+	{
+		this.users = user;
+	}
+	
+	addUser(event, context)
+	{
+		let user = JSON.parse(event.body);
+		this.users.addUser(user).subscribe(
+		{
+			
+		});
+	}
+	
+	deleteUser(event, context)
+	{
+		
+	}
+	
+	getUser(event, context)
+	{
+		
+	}
+	
+	getUserList(event, context)
+	{
+		
+	}
+	
+	updateUser(event, context)
+	{
 
+	}	
+}
+
+function next()
+{
+	
 }
 
 module.exports = UsersService;
