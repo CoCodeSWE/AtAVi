3efa85d77ea397a8e3666d24f14d86da3c6b1077
sosssoldi <angus_67@live.it>
2017-04-29 16:14:07 +0200

    Classi Logic commentate secondo lo standard

--- src/Client/Logic/Logic.js
+++ src/Client/Logic/Logic.js
@@ -3,18 +3,28 @@ const URL = 'www.google.it'
 
 class Logic
 {
+  /**
+  * Costruttore della parte logica del sistema che si occupa di comunicare con l'API Gateway.
+  */
   constructor()
   {
     this.subject = new Rx.Subject();
   }
 
+  /**
+  * Metodo utilizzato per ottenere un Observable per l'arrivo dei dati dall'API Gateway.-
+  */
   getObservable()
   {
     return this.subject.asObservable();
   }
 
-  sendData(blob)
+  /**
+  * Metodo che permette di inviare l'audio all'API Gateway.
+  * @param audio {Blob} - Parametro contenente l'audio da inviare all'APIGateway.
+  */
+  sendData(audio)
   {
-    return new HttpPromise('POST', URL, 'test', blob);
+    return new HttpPromise('POST', URL, 'test', audio);
   }
 }
