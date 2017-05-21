cd220ec82bd1941621fb10ee8593c11ac123e3e3
Scramasax <scramasax443@gmail.com>
2017-05-21 16:38:40 +0200

    fix curiosity webhook + handler

--- src/Back-end/CuriosityWebhookService/handler.js
+++ src/Back-end/CuriosityWebhookService/handler.js
@@ -1,10 +1,12 @@
 const CuriosityWebhookService = require('./CuriosityWebhookService');
 const CuriosityDAO = require('./CuriosityDAODynamoDB');
+const GuestsDAO = require('../Guests/GuestsDAODynamoDB');
 const aws = require('aws-sdk');
 
 let client = new aws.DynamoDB.DocumentClient();
 let curiosity = new CuriosityDAO(client);
+let guests = new GuestsDAO(guests);
 
-let service = new CuriosityWebhookService(curiosity);
+let service = new CuriosityWebhookService(curiosity,guests);
 
 module.exports.webhook = service.webhook.bind(service);
